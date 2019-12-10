f485bf43848032fb4a8197e5203aa81103e5b16f
Artem Oharkov <aoharkov@kse.org.ua>
2019-12-10 23:34:27 +0200

    Task 2.2 create base class Book

---
+++ src/main/java/task2/p2/Book.java
@@ -0,0 +1,67 @@
+package task2.p2;
+
+public class Book {
+    private String bookName;
+    private String authorName;
+    private String publisherName;
+    private int yearOfPublishing;
+    private int numberOfPages;
+    private int price;
+
+    public Book(String bookName, String authorName, String publisherName, int yearOfPublishing, int numberOfPages, int price) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.publisherName = publisherName;
+        this.yearOfPublishing = yearOfPublishing;
+        this.numberOfPages = numberOfPages;
+        this.price = price;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
