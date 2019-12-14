b30c9c42ef12687cd620e0206728a0c1c827aff3
Artem Oharkov <aoharkov@kse.org.ua>
2019-12-14 18:53:53 +0200

    Interview Questions: OOP

---
+++ src/main/java/interviewquestions/OOP.java
@@ -0,0 +1,79 @@
+package interviewquestions;
+
+public class OOP {
+    public static void main(String[] args) {
+        question1();
+    }
+
+    private static void question1() {
+        A a = new B();
+        System.out.println(a.name);
+        System.out.println(a.staticName);
+        System.out.println(a.getName());
+        System.out.println(a.getStaticName());
+    }
+}
+
+class A {
+    String name = "nameA";
+    String staticName = "staticNameA";
+
+    A() {
+        System.out.println("Constructor A");
+        print();
+        printPrivate();
+    }
+
+    public void print() {
+        System.out.println("A.print()");
+        print2();
+    }
+
+    public void print2() {
+        System.out.println("A.print2()");
+    }
+
+    private void printPrivate() {
+        System.out.println("A.printPrivate()");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStaticName() {
+        return staticName;
+    }
+}
+
+class B extends A {
+    String name = "nameB";
+    String staticName = "staticNameB";
+
+    B() {
+        System.out.println("Constructor B");
+        print();
+        printPrivate();
+    }
+
+    public void print() {
+        System.out.println("B.print()");
+        print2();
+    }
+
+    public void print2() {
+        System.out.println("B.print2()");
+    }
+
+    private void printPrivate() {
+        System.out.println("B.printPrivate()");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStaticName() {
+        return staticName;
+    }
+}
