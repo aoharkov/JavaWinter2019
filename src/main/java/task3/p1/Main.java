a4aabe16fbccc01e9095044478ec8311af9d9573
Artem Oharkov <aoharkov@kse.org.ua>
2019-12-16 13:14:36 +0200

    Task 3.1: update Toy

--- src/main/java/task3/p1/Main.java
+++ src/main/java/task3/p1/Main.java
@@ -1,9 +1,13 @@
 package task3.p1;
 
 import task3.p1.controller.ToyController;
+import task3.p1.model.domain.Toy;
 
 public class Main {
     public static void main(String[] args) {
         ToyController toyController = new ToyController();
+        System.out.println(new Toy());
+        System.out.println(new Toy());
+        System.out.println(new Toy());
     }
 }
--- src/main/java/task3/p1/model/domain/Toy.java
+++ src/main/java/task3/p1/model/domain/Toy.java
@@ -1,4 +1,48 @@
 package task3.p1.model.domain;
 
 public class Toy {
+    protected static int counter = 0;
+    private static int toyCounter = 0;
+
+    private int id;
+    private String name;
+
+    public Toy() {
+        counter++;
+        incToyCounter();
+        this.id = counter;
+        this.name = getClass().getSimpleName() + getToyCounter();
+    }
+
+    protected void incToyCounter() {
+        toyCounter++;
+    }
+
+    protected int getToyCounter() {
+        return toyCounter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Toy{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
