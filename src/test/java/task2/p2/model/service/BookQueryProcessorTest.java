eb48278e3660f65890bd9c4b56319de92a9e73d3
Artem Oharkov <aoharkov@kse.org.ua>
2019-12-29 00:47:05 +0200

    test(task 4.2): parameterize test for BookQueryProcessor

--- src/test/java/task2/p2/model/service/BookQueryProcessorTest.java
+++ src/test/java/task2/p2/model/service/BookQueryProcessorTest.java
@@ -1,6 +1,7 @@
 package task2.p2.model.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import task2.p2.model.domain.Book;
 import task2.p2.model.domain.BookField;
@@ -13,45 +14,34 @@ import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class BookQueryProcessorTest {
     private Book[] books = BookArrayGenerator.generate(6);
-/*    @Parameterized.Parameter
+    @Parameterized.Parameter
     public Book[] expected;
     @Parameterized.Parameter(1)
     public QueryType queryType;
     @Parameterized.Parameter(2)
     public BookField bookField;
     @Parameterized.Parameter(3)
-    public String name;
+    public String[] args;
 
 
     @Parameterized.Parameters
     public static Collection data() {
         return Arrays.asList(new Object[][]{
-                {{BookGenerator.generate(1), BookGenerator.generate(5)}, QueryType.GET, BookField.AUTHOR_NAME, "Author1"},
-                {{BookGenerator.generate(1), BookGenerator.generate(5)}, QueryType.GET, BookField.PUBLISHER_NAME, "Publisher2"}
+                {new Book[]{BookGenerator.generate(1), BookGenerator.generate(5)},
+                        QueryType.GET, BookField.AUTHOR_NAME, new String[]{"Author1"}},
+                {new Book[]{BookGenerator.generate(2), BookGenerator.generate(5)},
+                        QueryType.GET, BookField.PUBLISHER_NAME, new String[]{"Publisher2"}}
         });
-    }*/
-
-    @Test
-    public void process() {
-        Book[] expected = new Book[2];
-        expected[0] = BookGenerator.generate(1);
-        expected[1] = BookGenerator.generate(5);
-        assertArrayEquals(expected,
-                BookQueryProcessor.process(
-                        books, QueryType.GET,
-                        BookField.AUTHOR_NAME, "Author1"));
     }
 
     @Test
-    public void process2() {
-        Book[] expected = new Book[2];
-        expected[0] = BookGenerator.generate(2);
-        expected[1] = BookGenerator.generate(5);
+    public void process() {
         assertArrayEquals(expected,
                 BookQueryProcessor.process(
-                        books, QueryType.GET,
-                        BookField.PUBLISHER_NAME, "Publisher2"));
+                        books, queryType,
+                        bookField, args));
     }
 }
