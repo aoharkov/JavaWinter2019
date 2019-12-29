e18139a6d2350ad1a5224a4b72f237ba490f5241
Artem Oharkov <aoharkov@kse.org.ua>
2019-12-29 17:27:00 +0200

    refactor(task 2.2): delete unused getters & setters
    getters - because they duplicate getters by query
    setters -are redundant for the things which do not change after creation

--- src/main/java/task2/p2/model/domain/Book.java
+++ src/main/java/task2/p2/model/domain/Book.java
@@ -59,46 +59,6 @@ public class Book {
                 bookName, authorName, publisherName, yearOfPublishing, numberOfPages, price);
     }
 
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
-    public String getPublisherName() {
-        return publisherName;
-    }
-
-    public void setPublisherName(String publisherName) {
-        this.publisherName = publisherName;
-    }
-
-    public int getYearOfPublishing() {
-        return yearOfPublishing;
-    }
-
-    public void setYearOfPublishing(int yearOfPublishing) {
-        this.yearOfPublishing = yearOfPublishing;
-    }
-
-    public int getNumberOfPages() {
-        return numberOfPages;
-    }
-
-    public void setNumberOfPages(int numberOfPages) {
-        this.numberOfPages = numberOfPages;
-    }
-
     public int getPrice() {
         return price;
     }
