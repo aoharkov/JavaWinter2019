ebed2459fa54c33e3ab00d3fadb2f0fa70ef6c89
Artem Oharkov <aoharkov@kse.org.ua>
2020-01-04 11:09:59 +0200

    feat(task 5.1): add domain

--- src/main/java/task5/p1/model/domain/LogEntry.java
+++ src/main/java/task5/p1/model/domain/LogEntry.java
@@ -1,5 +1,81 @@
 package task5.p1.model.domain;
 
-public class LogEntry {
+import java.util.Objects;
 
+public class LogEntry {
+    private String surname;
+    private String name;
+    private String dateOfBirth;
+    private String telephoneNumber;
+    private String homeAddress;
+
+    public LogEntry(String surname, String name, String dateOfBirth, String telephoneNumber, String homeAddress) {
+        this.surname = surname;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.telephoneNumber = telephoneNumber;
+        this.homeAddress = homeAddress;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LogEntry)) return false;
+        LogEntry logEntry = (LogEntry) o;
+        return surname.equals(logEntry.surname) &&
+                name.equals(logEntry.name) &&
+                dateOfBirth.equals(logEntry.dateOfBirth) &&
+                Objects.equals(telephoneNumber, logEntry.telephoneNumber) &&
+                Objects.equals(homeAddress, logEntry.homeAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(surname, name, dateOfBirth, telephoneNumber, homeAddress);
+    }
+
+    @Override
+    public String toString() {
+        return surname + ", " + name + ", " + dateOfBirth + ", " + telephoneNumber + ", " + homeAddress;
+    }
 }
