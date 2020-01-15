6bc8265dc9248bf9b9c9c8e902a8ed62d2d958ee
Artem Oharkov <aoharkov@kse.org.ua>
2020-01-15 13:43:14 +0200

    feat(tester): implement @After, @BeforeAll and @AfterAll

--- src/main/java/practice/lesson1/tester/MyTest.java
+++ src/main/java/practice/lesson1/tester/MyTest.java
@@ -1,13 +1,47 @@
 package practice.lesson1.tester;
 
+import practice.lesson1.tester.annotations.After;
+import practice.lesson1.tester.annotations.AfterAll;
+import practice.lesson1.tester.annotations.Before;
+import practice.lesson1.tester.annotations.BeforeAll;
+import practice.lesson1.tester.annotations.Test;
+
 public class MyTest {
-    @NewTest
+    @BeforeAll
+    public void beforeAll1() {
+        System.out.println("================\nbeforeAll 1\n================");
+    }
+    @Before
+    public void before1() {
+        System.out.println("before 1");
+    }
+
+    @After
+    public void after1() {
+        System.out.println("after 1\n----------------");
+    }
+
+    @AfterAll
+    public void afterAll1() {
+        System.out.println("afterAll 1\n================");
+    }
+
+    @Test
     public void test1() {
         System.out.println("test1");
     }
 
-    @NewTest
+    @Test
     public void test2() {
         System.out.println("test2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("test3");
+    }
+
+    private void noTest() {
+        System.out.println("not a test");
     }
 }
--- src/main/java/practice/lesson1/tester/TestRunner.java
+++ src/main/java/practice/lesson1/tester/TestRunner.java
@@ -1,31 +1,52 @@
 package practice.lesson1.tester;
 
+import practice.lesson1.tester.annotations.After;
+import practice.lesson1.tester.annotations.AfterAll;
+import practice.lesson1.tester.annotations.Before;
+import practice.lesson1.tester.annotations.BeforeAll;
+import practice.lesson1.tester.annotations.Test;
+
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestRunner {
     public void run(Class<?> clazz) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         Constructor<?> constructor = clazz.getConstructor();
         Object object = constructor.newInstance();
         Method[] methods = clazz.getDeclaredMethods();
-        //List<Method> testMethods = getMethodsByAnnotation(NewTest.class, methods);
-        //List<Method> beforeMethods = getMethodsByAnnotation(NewBefore.class, methods);
-        for (Method method:methods) {
-            //not finished
-            if (method.isAnnotationPresent(NewTest.class)) {
-                method.invoke(object);
+        List<Method> testMethods = getMethodsByAnnotation(Test.class, methods);
+        List<Method> beforeMethods = getMethodsByAnnotation(Before.class, methods);
+        List<Method> afterMethods = getMethodsByAnnotation(After.class, methods);
+        List<Method> beforeAllMethods = getMethodsByAnnotation(BeforeAll.class, methods);
+        List<Method> afterAllMethods = getMethodsByAnnotation(AfterAll.class, methods);
+        for (Method beforeAllMethod : beforeAllMethods) {
+            beforeAllMethod.invoke(object);
+        }
+        for (Method testMethod : testMethods) {
+            for (Method beforeMethod : beforeMethods) {
+                beforeMethod.invoke(object);
             }
+            testMethod.invoke(object);
+            for (Method afterMethod : afterMethods) {
+                afterMethod.invoke(object);
+            }
+        }
+        for (Method afterAllMethod : afterAllMethods) {
+            afterAllMethod.invoke(object);
         }
     }
 
-//    private List<Method> getMethodsByAnnotation(Class<?> annotation, Method[] methods) {
-//        List<Method> tempMethods = new ArrayList<>();
-//        for (Method method :
-//                methods) {
-//            if (method.isAnnotationPresent(annotation)) {
-//
-//            }
-//        }
-//    }
+    private List<Method> getMethodsByAnnotation(Class<? extends Annotation> annotation, Method[] methods) {
+        List<Method> tempMethods = new ArrayList<>();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(annotation)) {
+                tempMethods.add(method);
+            }
+        }
+        return tempMethods;
+    }
 }
