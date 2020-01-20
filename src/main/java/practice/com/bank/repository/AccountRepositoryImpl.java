2f569da5d003ff61cca91ccf686966bd2952696f
Artem Oharkov <aoharkov@kse.org.ua>
2020-01-20 16:53:15 +0200

    feat(AccountRepositoryImpl): implement methods

--- src/main/java/practice/com/bank/repository/AccountRepositoryImpl.java
+++ src/main/java/practice/com/bank/repository/AccountRepositoryImpl.java
@@ -2,34 +2,42 @@ package practice.com.bank.repository;
 
 import practice.com.bank.domain.Account;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AccountRepositoryImpl implements AccountRepository {
-    private final Map<Integer, Account> accountIdToAccount = new HashMap<>();
+    private final Map<Integer, Account> accountIdToAccount;
+
+    public AccountRepositoryImpl() {
+        accountIdToAccount = new HashMap<>();
+    }
+
     @Override
     public void save(Account entity) {
-
+        accountIdToAccount.put(entity.getId(), entity);
     }
 
     @Override
     public Account findById(Integer id) {
-        return null;
+        return accountIdToAccount.getOrDefault(id, null);
     }
 
     @Override
     public List<Account> findAll() {
-        return null;
+        return new ArrayList<>(accountIdToAccount.values());
     }
 
     @Override
     public void update(Account entity) {
-
+        if (accountIdToAccount.containsKey(entity.getId())) {
+            accountIdToAccount.put(entity.getId(), entity);
+        }
     }
 
     @Override
     public void deleteById(Integer id) {
-
+        accountIdToAccount.remove(id);
     }
 }
